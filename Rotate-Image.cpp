c7752164519ab2e5ab5079e776014199831ba074
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-04-06 02:30:19 +0530

    added bubble sort

--- Rotate-Image.cpp
+++ Rotate-Image.cpp
@@ -1,23 +1,25 @@
 #include<iostream>
 using namespace std;
-int main() {
-	int a[1000];
-	int i, n;
-	cin >> n;
-	for (i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	int key;
-	cin >> key;
-	for (i = 0; i < n; i++) {
-		if (a[i] == key) {
-			cout << i;
-			break;
+
+void bubbleSort(int a[], int n) {
+	for (int itr = 0; itr < n; itr++) {
+		for (int j = 0; j < n - itr - 1; j++) {
+			if (a[j] > a[j + 1]) {
+				swap(a[j], a[j + 1]);
+			}
 		}
 	}
-	if (i == n) {
-		cout << "-1";
-
+}
+int main() {
+	int n;
+	cin >> n;
+	int a[10000];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	bubbleSort(a, n);
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << endl;
 	}
 	return 0;
 }
---
+++ bubble-sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int a[], int n){
+	for(int itr=0;itr<n;itr++){
+		for(int j=0;j<n-itr-1;j++){
+			if(a[j]>a[j+1]){
+				swap(a[j],a[j+1]);
+			}
+		}
+	}
+}
+int main() {
+	int n;
+	cin>>n;
+	int a[10000];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	bubbleSort(a,n);
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<endl;
+	}
+	return 0;
+}
