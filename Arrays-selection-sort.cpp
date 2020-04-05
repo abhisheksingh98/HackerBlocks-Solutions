662777744f2f4d67c399d5ce38b2c754288f85d8
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-04-05 22:39:19 +0530

    Initial Commit

---
+++ Arrays-selection-sort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void selection_sort(int a[], int n){
+	for(int i=0;i<n-1;i++){
+		int min_index = i;
+		for(int j=i;j<n;j++){
+			if(a[j]<a[min_index]){
+			min_index=j;
+			}
+		}
+		swap(a[i],a[min_index]);
+	}
+}
+int main() {
+	int n;
+	int a[10000];
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	selection_sort(a,n);
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<endl;
+	}
+	return 0;
+}
