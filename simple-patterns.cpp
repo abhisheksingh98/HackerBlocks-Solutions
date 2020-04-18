fededb9196e48b2d0f343f5edf979bbdc7f0ea00
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-04-19 04:40:28 +0530

    pattern 2

--- simple-patterns.cpp
+++ simple-patterns.cpp
@@ -19,3 +19,25 @@ int main(){
     }
     return 0;
 }
+
+// Pattern 2
+// 1
+// 1 2
+// 3 4 5
+// 6 7 8 9
+// 10 11 12 13
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,val=1;
+    cin>>n;
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=row;col++){
+            cout<<val++<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
