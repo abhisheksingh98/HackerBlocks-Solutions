e8a2c1c2432eb0bc0db615cc870d0d94c0375902
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-04-25 21:30:18 +0530

    added print primes

---
+++ print-primes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, n;
+    cin >> n;
+    for (i = 2; i <= n; i++)
+    {
+        for (j = 2; j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                break;
+            }
+        }
+        if (i == j)
+        {
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
