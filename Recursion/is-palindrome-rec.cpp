18a2b4c694a4bc6aa853ae6657dc2aca1323069d
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-07-04 21:59:03 +0530

    isPalindrome updated

--- Recursion/is-palindrome-rec.cpp
+++ Recursion/is-palindrome-rec.cpp
@@ -4,12 +4,19 @@ using namespace std;
 bool isPalindrome( int arr[ ] ,int i, int j) {
 
     if(i >= j) {
+          //Check if i>=j 
+        //In case of odd elements , i would become equal to j at the middle element and we can stop our search there
+        //In case of even elements , i would eventually become 1 more than j and if array was palindromic till here , we need not check further and can return true
        return true;
     }
     if(arr[i]!=arr[j]){
+        //Return false if ith and jth integer don't match
+
         return false;
     }
     else {
+         //Otherwise check whether the remaining array is palindromic or not
+
         return isPalindrome(arr,i+1,j-1) ;
     }
 }
