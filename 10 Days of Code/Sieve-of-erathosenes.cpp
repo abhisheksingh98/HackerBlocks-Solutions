1fb2ad9025fef3bdf778f5382975f5baa9dddee0
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-07-18 01:54:19 +0530

    added prime sieve

---
+++ 10 Days of Code/Sieve-of-erathosenes.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+//Sieve Approach - Generate an array containing prime numbers
+void primeSieve(int a[]){
+
+	//first mark all odd numbers prime
+	for(int i=3;i<1000000;i+=2){
+		a[i]=1;
+	}
+
+	//Sieve
+	for(long long i=3;i<1000000;i+=2){
+		//if the current number is not marked (it is prime)
+		if(a[i]==1){
+			//mark all the multiples of i as not prime
+			for(long long j=i*i;j<1000000;j+=i){
+				a[j]=0;
+			}
+		}
+	}
+
+	//special case
+	a[2] =1;
+	a[1]=a[0] = 0;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    int a[1000000] = {0};
+    primeSieve(a);
+
+    //lets print primes upto range n
+    for(int i=0;i<=n;i++){
+        if(p[i]==1){
+            cout<<i<<", ";
+        }
+    }
+    return 0;
+}
