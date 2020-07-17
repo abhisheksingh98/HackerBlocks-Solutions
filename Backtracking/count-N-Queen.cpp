9ba47c6796b3132ba03a8086149c61ffbc798b2f
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-07-17 20:06:41 +0530

    added count n queen

--- Backtracking/count-N-Queen.cpp
+++ Backtracking/count-N-Queen.cpp
@@ -1,7 +1,64 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    
-    return 0;
+bool isSafe(int board[][11], int row, int col, int n){
+	for(int i=row;i>=0;i--){
+		if(board[i][col]==1){
+			return false;
+		}
+	}
+
+	//Left Diagonals
+	int x = row;
+	int y = col;
+	
+	while(x>=0 && y>=0){
+		if(board[x][y]==1){
+			return false;
+		}
+		x--;
+		y--;
+	}
+
+	//Loop has ended
+
+	//Right Diagonals
+	x=row;
+	y=col;
+
+	while(x>=0 && y<n){
+		if(board[x][y]==1){
+			return false;
+		}
+		x--;
+		y++;
+	}
+	return true;
+}
+
+int countNQueen(int board[][11],int row, int n){
+	//base case
+	if(row==n){
+		return 1;
+	}
+	int count = 0;
+	for(int col=0;col<n;col++){
+		if(isSafe(board,row,col,n)){
+			board[row][col] = 1; //my work
+
+			count+=countNQueen(board,row+1,n);
+			board[row][col] = 0; //backtracking
+		}
+	}
+	return count;
+}
+
+int main() {
+	int n;
+	cin>>n;
+	int board[11][11]={0};
+	
+	cout<<countNQueen(board,0,n);
+
+	return 0;
 }
